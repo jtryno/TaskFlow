fc44679facc07e1da88a09706dd17ef11f79076a
jtryno <jtrinter1@gmail.com>
2026-03-22 16:50:55 -0400

    Added tests for the remaining API endpoints

--- tests/test_tasks.py
+++ tests/test_tasks.py
@@ -74,4 +74,94 @@ def test_list_all_tasks(client):
     assert "created_at" in data[0]
 
 
+# Test 3: Get a single task by task_id PASS  |  GET /tasks/{task_id}
+# Expected result: 200 Status Code, initially an empty list,
+# then a list with the single task that has the task_id.
+def test_get_task_by_id_pass(client):
+    initial_response = client.get("/tasks")
+    assert initial_response.status_code == 200
+
+    post_response = client.post("/tasks", json={"title": "Test Task", "description": "A test"})
+    assert post_response.status_code == 201
+
+    get_response = client.get("/tasks/1")
+    assert get_response.status_code == 200
+
+    data = get_response.json()
+
+    assert data["title"] == "Test Task"
+    assert data["description"] == "A test"
+    assert data["completed"] is False
+    assert "id" in data
+    assert "created_at" in data
+
+
+# Test 4: Get a single task by id FAIL  |  GET /tasks/{task_id}
+# Expected result: 404 Status Code upon looking for a task
+# where the task_id doesn't exist.
+def test_get_task_by_id_fail(client):
+    initial_response = client.get("/tasks")
+    assert initial_response.status_code == 200
+
+    get_response = client.get("/tasks/9999")
+    assert get_response.status_code == 404
+
+    data = get_response.json()
+    assert data["detail"] == "Task not found"
+
+
+# Test 5: Update a task via task id PASS  |  PUT /tasks/{task_id}
+# Expected result: 200 Status Code with the updated fields
+# in the JSON body.
+def test_update_single_task_pass(client):
+    post_response = client.post("/tasks", json={"title": "Test Task", "description": "A test"})
+    assert post_response.status_code == 201
+
+    put_response = client.put("/tasks/1", json={"title": "NEW Test Task", "description": "NEW description", "completed": True})
+    assert put_response.status_code == 200
+
+    new_data = put_response.json()
+    assert new_data["title"] == "NEW Test Task"
+    assert new_data["description"] == "NEW description"
+    assert new_data["completed"] is True
+    assert "id" in new_data
+    assert "created_at" in new_data
+
+
+# Test 6: Update a task via task id FAIL  |  PUT /tasks/{task_id}
+# Expected result: 404 Status Code for a task where task_id is
+# not found.
+def test_update_single_task_fail(client):
+    put_response = client.put("/tasks/9999", json={"title": "NEW Test Task", "description": "NEW description", "completed": True})
+    assert put_response.status_code == 404
+
+    new_data = put_response.json()
+    assert new_data["detail"] == "Task not found"
+
+
+# Test 7: Delete a task via task id PASS  |  DELETE /tasks/{task_id}
+# Expected result: 204 Status with no response body
+# Checks GET /tasks/{task_id} to ensure a 404 response
+def test_delete_task_pass(client):
+    post_response = client.post("/tasks", json={"title": "Test Task", "description": "A test"})
+    assert post_response.status_code == 201
+
+    delete_response = client.delete("/tasks/1")
+    assert delete_response.status_code == 204
+    
+    get_response = client.get("/tasks/1")
+    assert get_response.status_code == 404
+
+    data = get_response.json()
+    assert data["detail"] == "Task not found"
+
+
+# Test 8: Delete a task via task id FAIL  |  DELETE /tasks/{task_id}
+# Expected result: 404 Status Code, task to be deleted couldn't be found.
+def test_delete_task_fail(client):
+    delete_response = client.delete("/tasks/9999")
+    assert delete_response.status_code == 404
+
+    data = delete_response.json()
+    assert data["detail"] == "Task not found"
 
