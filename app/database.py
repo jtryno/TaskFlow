48f08587f7e9ac2080f09a5fb3b0d46929995628
jtryno <jtrinter1@gmail.com>
2026-03-23 19:30:57 -0400

    Added environment variable support with pydantic-settings

--- app/database.py
+++ app/database.py
@@ -1,11 +1,12 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
+from app.config import settings
 
-SQLALCHEMY_DATABASE_URL = "sqlite:///./tasks.db"
+SQLALCHEMY_DATABASE_URL = settings.database_url
 
-engine = create_engine(
-    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
-)
+connect_args = {"check_same_thread": False} if settings.database_url.startswith("sqlite") else {}
+
+engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args=connect_args)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
