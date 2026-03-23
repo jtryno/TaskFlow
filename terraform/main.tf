8bbc26167ae3a4ecc5679bad67f0394401b55e35
jtryno <jtrinter1@gmail.com>
2026-03-22 22:28:43 -0400

    Added Terraform configuration for the existing AWS infrastructure

---
+++ terraform/main.tf
@@ -0,0 +1,64 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 6.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_security_group" "security-group" {
+  name        = "taskflow-sg"
+  description = "taskflow-sg created 2026-03-22T23:24:35.180Z"
+
+  ingress {
+    description = "SSH Connection"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "Public access to the API"
+    from_port   = 8000
+    to_port     = 8000
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "TaskFlow"
+  }
+
+}
+
+resource "aws_instance" "taskflow-server" {
+  ami                    = "ami-02dfbd4ff395f2a1b"
+  instance_type          = "t2.micro"
+  key_name               = "taskflow-key"
+  vpc_security_group_ids = [aws_security_group.security-group.id]
+
+  tags = {
+    Name = "TaskFlow"
+  }
+}
+
+resource "aws_eip" "taskflow-ec2-ip" {
+  instance = aws_instance.taskflow-server.id
+
+  tags = {
+    Name = "TaskFlow"
+  }
+}
